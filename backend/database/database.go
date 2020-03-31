9e029edb166d537386b637bfc06824599081aaff
Kyle <mcgoughboy@gmail.com>
2020-03-30 20:49:29 -0700

    oops accidentally ignored the backend folder

---
+++ backend/database/database.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres" // enables the use of Postgresql with Gorm
+	"github.com/tokengame/backend/database/models"
+)
+
+// GetDatabaseConnection returns a gorm.DB pointer or error if one occurred
+func GetDatabaseConnection() (*gorm.DB, error) {
+	return gorm.Open("postgres", "host=database port=5432 user=postgres password=TokenTournamentSecretPW dbname=postgres sslmode=disable")
+}
+
+// RunDatabaseMigrations run's GORM automigrations on the database models
+// ensuring that the database table matches the struct fields
+func RunDatabaseMigrations() error {
+	db, err := GetDatabaseConnection()
+	if err != nil {
+		return err
+	}
+
+	db.AutoMigrate(&models.Challenge{})
+	return nil
+}
