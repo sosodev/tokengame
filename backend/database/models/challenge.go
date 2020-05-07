c6f14bf79ab12e7f814f500732fb8b3034302434
Felix Ferreira <13thfinance@gmail.com>
2020-05-07 16:47:47 -0700

    Challenges no one will look at or implement

--- backend/database/models/challenge.go
+++ backend/database/models/challenge.go
@@ -154,5 +154,141 @@ Return the n index number, fib(4) should return 3
 		},
 	})
 
+	db.Create(&Challenge{
+		Title: "Depth First Search",
+		Head: `/**/
+		function depthFirstSearch(out) {`,
+		Foot: "return out;\n}",
+		Tokens: []string{
+			"{",
+			"}",
+		},
+		Testcases: []string{
+			"function test1() {return false;} test1();",
+		},
+	})
+
+	db.Create(&Challenge{
+		Title: "Bredth First Search",
+		Head: `/**/
+		function breathFirstSearch(out) {`,
+		Foot: "return out;\n}",
+		Tokens: []string{
+			"{",
+			"}",
+		},
+		Testcases: []string{
+			"function test1() {return false;} test1();",
+		},
+	})
+
+	db.Create(&Challenge{
+		Title: "Summation of Primes",
+		Head: `/**/
+		function test(out) {`,
+		Foot: "return out;\n}",
+		Tokens: []string{
+			"{",
+			"}",
+		},
+		Testcases: []string{
+			"function test1() {return false;} test1();",
+		},
+	})
+
+	db.Create(&Challenge{
+		Title: "Power Digit Sum",
+		Head: `/**/
+		function test(out) {`,
+		Foot: "return out;\n}",
+		Tokens: []string{
+			"{",
+			"}",
+		},
+		Testcases: []string{
+			"function test1() {return false;} test1();",
+		},
+	})
+
+	db.Create(&Challenge{
+		Title: "10001st Prime",
+		Head: `/**/
+		function test(out) {`,
+		Foot: "return out;\n}",
+		Tokens: []string{
+			"{",
+			"}",
+		},
+		Testcases: []string{
+			"function test1() {return false;} test1();",
+		},
+	})
+
+	db.Create(&Challenge{
+		Title: "Pandigital Numbers",
+		Head: `/**/
+		function test(out) {`,
+		Foot: "return out;\n}",
+		Tokens: []string{
+			"{",
+			"}",
+		},
+		Testcases: []string{
+			"function test1() {return false;} test1();",
+		},
+	})
+	db.Create(&Challenge{
+		Title: "Self Powers",
+		Head: `/**/
+		function test(out) {`,
+		Foot: "return out;\n}",
+		Tokens: []string{
+			"{",
+			"}",
+		},
+		Testcases: []string{
+			"function test1() {return false;} test1();",
+		},
+	})
+
+	db.Create(&Challenge{
+		Title: "Prime Permutations",
+		Head: `/**/
+		function test(out) {`,
+		Foot: "return out;\n}",
+		Tokens: []string{
+			"{",
+			"}",
+		},
+		Testcases: []string{
+			"function test1() {return false;} test1();",
+		},
+	})
+
+	db.Create(&Challenge{
+		Title: "Distinct Colourings of a Rubik's Cube",
+		Head: `/*The well-known Rubik's Cube puzzle has many fascinating mathematical properties. The 2×2×2 variant has 8 cubelets with a total of 24 visible faces, each with a coloured sticker. Successively turning faces will rearrange the cubelets, although not all arrangements of cubelets are reachable without dismantling the puzzle.
+
+Suppose that we wish to apply new stickers to a 2×2×2 Rubik's cube in a non-standard colouring. Specifically, we have n different colours available (with an unlimited supply of stickers of each colour), and we place one sticker on each of the 24 faces in any arrangement that we please. We are not required to use all the colours, and if desired the same colour may appear in more than one face of a single cubelet.
+
+We say that two such colourings c1,c2 are essentially distinct if a cube coloured according to c1 cannot be made to match a cube coloured according to c2 by performing mechanically possible Rubik's Cube moves.
+
+For example, with two colours available, there are 183 essentially distinct colourings.
+
+How many essentially distinct colourings are there with 10 different colours available?
+
+*/
+		function test(out) {`,
+		Foot: "return out;\n}",
+		Tokens: []string{
+			"{",
+			"}",
+		},
+		Testcases: []string{
+			"function test1() {return false;} test1();",
+		},
+	})
+
+
 	return db.Error
 }
