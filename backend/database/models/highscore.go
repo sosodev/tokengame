90e8f95368a2be4b8248d5615697b0845ed2c606
Kyle <mcgoughboy@gmail.com>
2020-05-08 12:17:16 -0700

    fix that thing quick

--- backend/database/models/highscore.go
+++ backend/database/models/highscore.go
@@ -11,3 +11,46 @@ type Highscore struct {
 	Score       int    `json:"score"`
 	ChallengeID uint   `json:"challenge_id"`
 }
+
+// SeedHighscores seeds the highscores table
+func SeedHighscores(db *gorm.DB) error {
+	highscore := &Highscore{}
+
+	err := db.First(highscore).Error
+	if !gorm.IsRecordNotFoundError(err) {
+		return err
+	}
+
+	if highscore.Nickname != "" {
+		return nil
+	}
+
+	db.Create(&Highscore{
+		Nickname:    "Cool Guy",
+		Score:       8420,
+		ChallengeID: 2,
+	})
+	if db.Error != nil {
+		return db.Error
+	}
+
+	db.Create(&Highscore{
+		Nickname:    "Chet",
+		Score:       5356,
+		ChallengeID: 1,
+	})
+	if db.Error != nil {
+		return db.Error
+	}
+
+	db.Create(&Highscore{
+		Nickname:    "Chet but fast",
+		Score:       7560,
+		ChallengeID: 1,
+	})
+	if db.Error != nil {
+		return db.Error
+	}
+
+	return db.Error
+}
